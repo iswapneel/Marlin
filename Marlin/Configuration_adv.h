61b534d99c3e1b991762b2154b2ffdd4d66d00db
Swapneel <73296785+iswapneel@users.noreply.github.com>
2021-05-26 19:27:47 +0530

    Lowered extruder current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2442,7 +2442,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      940        // iswap for tmc2208
+    #define E0_CURRENT      800        // iswap for tmc2208
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
